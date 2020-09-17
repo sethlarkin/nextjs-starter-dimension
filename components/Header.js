89058bdd7596279a05c2ea742d9e1c51c4ac70ad
sethlarkin <sethlarkin@gmail.com>
2020-09-17 13:34:18 -0700

    initial changes

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Seth Larkin</h1>
+                <p>Welcome to my Homepage. I am here to showcase my work and bring in new clients</p>
             </div>
         </div>
         <nav>
